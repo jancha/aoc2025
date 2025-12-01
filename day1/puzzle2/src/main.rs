11958377913494cc32d7130731b77fa26e3560bb
Janis Volbergs <janis.volbergs@gmail.com>
2025-12-01 08:46:25 +0200

    Cleanup

--- day1/puzzle2/src/main.rs
+++ day1/puzzle2/src/main.rs
@@ -3,7 +3,6 @@ use std::fs;
 
 pub fn solver(file_path: &str) -> Result<String> {
     let input = fs::read_to_string(file_path).expect("Could not read input file");
-
     let mut left_count = 0;
     let mut pos: isize = 50;
     let mut start_pos;
@@ -11,17 +10,13 @@ pub fn solver(file_path: &str) -> Result<String> {
         start_pos = pos;
         let dir = &i[0..1];
         let steps = i[1..].parse::<isize>().expect("Not a number");
-        let zeros = steps / 100;
-        left_count += zeros;
-
+        left_count += steps / 100;
         let steps = steps % 100;
-
         match dir {
             "L" => pos -= steps,
             "R" => pos += steps,
             _ => return Err(Box::new(AOCError::String("Invalid char".to_string()))),
         }
-
         let mut push_zero = false;
         if pos > 99 {
             push_zero = true;
@@ -30,12 +25,10 @@ pub fn solver(file_path: &str) -> Result<String> {
             push_zero = true;
             pos = 100 + pos % 100;
         }
-
         if pos == 0 || (push_zero && start_pos != 0) {
             left_count += 1;
         }
     }
-
     Ok(format!("{left_count}"))
 }
 

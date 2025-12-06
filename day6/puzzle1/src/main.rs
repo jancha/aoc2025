e784825e28f2b3f3f92447ceae5112e9171020b9
Janis Volbergs <janis.volbergs@gmail.com>
2025-12-06 15:04:57 +0200

    Cleanup for Day 6 - part 1

--- day6/puzzle1/src/main.rs
+++ day6/puzzle1/src/main.rs
@@ -9,46 +9,30 @@ pub fn solver(file_path: &str) -> Result<String> {
         let row = line.replace("  ", " ");
         let row = row.trim();
         let row = row
+            .trim()
             .split(" ")
             .filter(|&x| !x.is_empty())
             .collect::<Vec<&str>>();
         if row[0] == "+" || row[0] == "*" {
             row.iter().enumerate().for_each(|(i, x)| match *x {
                 "*" => {
-                    let mut row_val: usize = 0;
-                    rows.get(i).unwrap().iter().for_each(|x| {
-                        if row_val == 0 {
-                            row_val = *x;
-                        } else {
-                            row_val *= *x;
-                        }
-                    });
-
+                    let mut row_val: usize = 1;
+                    rows.get(i).unwrap().iter().for_each(|x| row_val *= *x);
                     sum += row_val;
                 }
                 "+" => {
-                    let mut row_val: usize = 0;
-                    rows.get(i).unwrap().iter().for_each(|x| {
-                        if row_val == 0 {
-                            row_val = *x;
-                        } else {
-                            row_val += *x;
-                        }
-                    });
-
-                    sum += row_val;
+                    rows.get(i).unwrap().iter().for_each(|x| sum += *x);
                 }
                 _ => panic!("Unsupported operator"),
             });
         } else {
             row.iter().enumerate().for_each(|(i, x)| {
-                let vec_row = if let Some(row) = rows.get_mut(i) {
-                    row
+                let x = x.parse::<usize>().unwrap();
+                if let Some(row) = rows.get_mut(i) {
+                    row.push(x);
                 } else {
-                    rows.push(Vec::new());
-                    rows.get_mut(i).unwrap()
+                    rows.push(vec![x]);
                 };
-                vec_row.push(x.parse::<usize>().unwrap());
             })
         }
     }
